6b15f3b4f0697c7fb3786fc2fbcbcdcdd79dff3b
nisha <annienm20@gmail.com>
2026-09-18 10:41:43 +0530

    add loading page

--- src/main.js
+++ src/main.js
@@ -1,19 +1,42 @@
 const barsicon = document.querySelector(".bars");
 const navlinks = document.querySelector(".nav-links");
-const wholecontent = document.querySelector("body");
+
 const darkmode = document.querySelector(".dark-mode");
 const prebtn = document.querySelector(".pre");
 const nextbtn = document.querySelector(".next");
 const slideContainer  = document.querySelector(".slide-container");
-
+const bodycontent = document.querySelector(".body-content")
 const slides = slideContainer.querySelectorAll(".slide");
-let totalSlides = slides.length;
+const loadingpage = document.querySelector(".loading-page");
+const loadingpagetext = document.querySelector(".loadingpage-text");
+let loadingtext = loadingpagetext.innerText;
+let totalSlides = loadingtext.length;
+let totalSlides1 = slides.length;
+let startnumber = 0;
+
+
+ 
+  const texttimer = setInterval(function(){
+    startnumber++;
+        loadingpagetext.innerText = loadingtext.substring(0, startnumber);
+        loadingpagetext.style.display = "block";
+    if(startnumber == totalSlides){
+       clearInterval(texttimer);
+       bodycontent.style.display = "block"; 
+       loadingpage.style.display = "none";
+       
+    };
+  },1000);
+ 
+
+
 
 barsicon.addEventListener("click", function() {
 navlinks.classList.toggle("nav-links-animation");
 });
 darkmode.addEventListener("click", function(){
-wholecontent.classList.toggle("darkmode-animation");
+document.body.classList.toggle("darkmode-animation");
+
 });
 let currentIndex = 0; // Tracks which slide is currently visible (0, 1, or 2)
 
@@ -23,7 +46,7 @@ function updateSlider() {
 }
 nextbtn.addEventListener("click", function() {
   currentIndex++;
-  if (currentIndex >= totalSlides) {
+  if (currentIndex >= totalSlides1) {
     currentIndex = 0; // Loop back to the first slide
   }
   updateSlider();
@@ -33,7 +56,7 @@ nextbtn.addEventListener("click", function() {
 prebtn.addEventListener("click", function() {
   currentIndex--;
   if (currentIndex < 0) {
-    currentIndex = totalSlides - 1; // Loop to the last slide
+    currentIndex = totalSlides1 - 1; // Loop to the last slide
   }
   updateSlider();
 });
